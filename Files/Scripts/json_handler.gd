e5e4a5d6211a5f4d8164ea5b9b43741f967d582b
PeraBajaDev <perabajadev@gmail.com>
2026-07-30 14:33:54 -0300

    fix: mejorado mensaje de error de archivo no encontrado

--- Files/Scripts/json_handler.gd
+++ Files/Scripts/json_handler.gd
@@ -5,9 +5,14 @@ class_name JSONHandler
 ## Returns a DataFile instance if given path is found, else returns null
 static func get_data_file(json_file_path: String) -> DataFile:
 	var text_data = FileAccess.get_file_as_string(json_file_path)
-	var json_data: Dictionary = JSON.parse_string(text_data)
-	if json_data == null or text_data.is_empty():
+	if text_data.is_empty():
+		if FileAccess.get_open_error() == Error.ERR_FILE_NOT_FOUND:
+			print("File was not found")
 		return null
+	var json_data: Dictionary = JSON.parse_string(text_data)
+	if json_data == null:
+		return null
+
 	var style: StringName = json_data[&"Style"]
 	var entries: Array[DialogueEntry] = []
 	for id in json_data[&"Dialogues"]:
