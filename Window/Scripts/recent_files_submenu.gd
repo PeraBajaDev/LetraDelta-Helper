02797944b8314bc505c64cc19cccc12a7673aeaf
PeraBajaDev <perabajadev@gmail.com>
2026-08-11 13:43:03 -0300

    fix: problemas con la metadata de la lista

--- Window/Scripts/recent_files_submenu.gd
+++ Window/Scripts/recent_files_submenu.gd
@@ -14,18 +14,19 @@ func _on_submenu_showed():
 	clear()
 	var recents_file_paths: PackedStringArray = FileAccess \
 			.get_file_as_string(RECENT_FILES_PATH) \
-			.split("\n")
+			.strip_edges(false).split("\n")
 	for i in range(len(recents_file_paths)):
 		if not recents_file_paths[i].is_absolute_path():
 			continue
 		add_item(recents_file_paths[i].get_file(), i + 2)
-		var item_index = get_item_index(i)
+		var item_index = get_item_index(i + 2)
 		set_item_tooltip(item_index, recents_file_paths[i])
 		set_item_metadata(item_index, recents_file_paths[i])
 
 	#For visual porpuses
-	add_separator()
+	add_separator("", 100)
 	add_item("Clear list", 0)
+	pass
 
 
 func _on_item_selected(index: int):
