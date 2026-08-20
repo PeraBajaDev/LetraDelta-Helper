c268cec7905556be9bca60255f3027149612b775
PeraBajaDev <perabajadev@gmail.com>
2026-08-20 09:52:37 -0300

    feat: añadido iconos de estados en la lista de entries

--- Entries/Scripts/entry_list.gd
+++ Entries/Scripts/entry_list.gd
@@ -2,6 +2,7 @@ class_name EntryList
 extends ItemList
 
 @export var _data_store: DataStore
+@export var _state_icons: Dictionary[Dialogue.State, Texture2D]
 
 
 func _ready() -> void:
@@ -15,7 +16,32 @@ func _on_data_loaded():
 	UIWatcher.watch(self, _data_store.entry_selected, _on_current_entry_changed)
 	for entry in _data_store.dialogues_entries:
 		var item_index = add_item(entry.id)
+		var state := _get_entry_state(entry)
+		set_item_icon(item_index, _state_icons[state])
 		set_item_metadata(item_index, entry)
+		for dialogue in entry.dialogues:
+			dialogue.content_changed.connect(_on_dialogue_content_changed.bind(dialogue, entry))
+			dialogue.needs_review_changed.connect(_on_dialogue_content_changed.bind(entry))
+
+
+func _on_dialogue_content_changed(_dialogue: Dialogue, entry: DialogueEntry):
+	var state := _get_entry_state(entry)
+	set_item_icon(_get_item_index_with_entry(entry), _state_icons[state])
+
+
+func _get_entry_state(entry: DialogueEntry) -> Dialogue.State:
+	var has_dialogue_with_state = func(dialogue: Dialogue, state: Dialogue.State):
+		return dialogue.get_state_report().state == state
+	for i in range(Dialogue.State.keys().size()):
+		var state := i as Dialogue.State
+		if state == Dialogue.State.TRANSLATED:
+			if entry.dialogues.all(has_dialogue_with_state.bind(state)):
+				return state
+			else:
+				continue
+		if entry.dialogues.any(has_dialogue_with_state.bind(state)):
+			return state
+	return Dialogue.State.NOT_TRANSLATED
 
 
 func _on_current_entry_changed(entry: DialogueEntry):
